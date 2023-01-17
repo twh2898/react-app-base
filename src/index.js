0f8afaf0423b9380ff91bd5f4cd4a19f55e1b8ea
Thomas Harrison <twh2898@vt.edu>
2023-01-16 20:24:54 -0500

    Add react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import WS, { Sub } from './WS';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ws" element={<WS />}>
+          <Route path="sub" element={<Sub />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
