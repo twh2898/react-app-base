341b8f5e5f872eef4512b17e79177958d30dcb2a
Thomas Harrison <twh2898@vt.edu>
2023-01-28 21:01:54 -0500

    Fix indent of WS.js

--- src/WS.js
+++ src/WS.js
@@ -1,21 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
 
 export function Sub() {
-    return (
-        <div>Sub</div>
-    );
+  return (
+    <div>Sub</div>
+  );
 }
 
 export function WS() {
-    return (
-        <div className="ws">
-            <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/ws/sub">Sub View</Link></li>
-            </ul>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className="ws">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/ws/sub">Sub View</Link></li>
+      </ul>
+      <Outlet />
+    </div>
+  );
 }
 
 export default WS;
